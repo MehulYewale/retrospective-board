93aca780f0f418716f4db47f61a4101ae7e20f3a
Mehul Yewale <yewalemehul@gmail.com>
2019-09-14 13:57:25 +0530

    Updates

---
+++ precache-manifest.0a34a0b95f9df64183e39bd284ab10c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5532d9fec75136f15d82",
+    "url": "/retrospective-board/static/css/main.fbc97f59.chunk.css"
+  },
+  {
+    "revision": "5532d9fec75136f15d82",
+    "url": "/retrospective-board/static/js/main.027ab9fc.chunk.js"
+  },
+  {
+    "revision": "f31b208ebb9aad878ab3",
+    "url": "/retrospective-board/static/js/runtime~main.9301c906.js"
+  },
+  {
+    "revision": "91247cbeeb0bbb404e47",
+    "url": "/retrospective-board/static/css/2.fdae2f6f.chunk.css"
+  },
+  {
+    "revision": "91247cbeeb0bbb404e47",
+    "url": "/retrospective-board/static/js/2.d2fac225.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/retrospective-board/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/retrospective-board/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/retrospective-board/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/retrospective-board/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/retrospective-board/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ab8536f467fcb12ed209f82c83c65493",
+    "url": "/retrospective-board/index.html"
+  }
+];
